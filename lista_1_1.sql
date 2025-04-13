5f4eb393592c790b7651dc62982c84727cc7e0ac
Rebeca Azevedo <azevedoreveca9@gmail.com>
2025-04-13 20:18:30 -0300

    Feat: Adiciona listas de exercícios

---
+++ lista_1_1.sql
@@ -0,0 +1,15 @@
+-- Lista de Exercícios 1.1 (basevendas fornecida pelo professor) 
+
+-- Use basevendas; 
+
+-- 1)Listar a descricao e o endereco de todos os fornecedores do estado SP, das cidades de DIADEMA e BARUERI 
+
+-- 2)Listar todos os campos de todos os fornecedores do estado SP, exceto os das cidades de BROTAS e SAO PAULO 
+
+-- 3)Listar a descricao e o telefone de todos os fornecedores dos estados MG e SP, cujo campo cidade esteja devidamente preenchido  
+
+-- 4)Listar todos os campos de todos os fornecedores, ordenando pela descricao, a partir do 5° registro, limitando a 8 linhas o resultado. 
+
+-- 5)Listar descricao, preço de custo e preço de venda (custo acrescido de 30%) de todos os produtos com estoque maior que 0 
+
+-- 6)Listar descricao, preço de custo,estoque e uma coluna chamada balanco que será resultado do custo, acrescido de 25% multiplicado pelo estoque de todos os produtos  
---
+++ lista_1_2.sql
@@ -0,0 +1,13 @@
+-- Lista de Exercícios 1.2 (basevendas fornecida pelo professor) 
+
+-- 1)Listar todos os campos de todos os produtos cujo custo acrescido de 30% seja maior do que 8 e menor que 10 
+
+-- 2)Listar todos os produtos ordenados pelo preço de custo (do mais caro para o mais barato) 
+
+-- 3)Listar todos os clientes com data de cadastro > 1° de outubro de 2002 
+
+-- 4)Listar todos os clientes com data de cadastro entre 1 e 16 de outubro de 2002 
+
+-- 5)Considerando que o lucro em cada venda é de 30%, listar todas as vendas cujo lucro tenha sido superior a 100,00 
+
+-- 6)Considerando que a comissão do vendedor é de 3% em cada venda , listar todas as vendas e a comissão a ser paga para cada venda. 
